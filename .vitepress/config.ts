f0802969365f52c12fa8f49ff5a6982b74a30c6d
vinzhao <870063335@qq.com>
2024-07-12 16:30:44 +0800

    部署

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   outDir:"docs",
+  base:"/docs",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
